bdaa9c5d2bd1fc9c980061117c83c2764d14a10f
maangelos <mariannaangelos.alves@gmail.com>
2022-04-24 17:42:48 -0300

    update controller to generic

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -1,36 +1,41 @@
 import prisma from '../prismaClient.js';
 
 class Controller {
+
+	constructor (entity) {
+		this.entity = entity;
+		this.prismaEntity = prisma[entity];
+	}
 	
 	async index (_request,response)  {
-		const patients = await prisma.patient.findMany();
-		response.json({patients});
+		const items = await this.prismaEntity.findMany();
+		response.json({'items': items});
 	}
 	
 	async getOne(request,response) {
 		const {id} = request.params;
 		
-		const patient = await prisma.patient.findUnique({
+		const item = await this.prismaEntity.findUnique({
 			where: {id}
 		});
-		response.json({'patient': patient});
+		response.json({'item': item});
 	}
 	
 	async store(request,response) {
 		const {body} = request;
-		const newPatient = await prisma.patient.create({data: body});
-		response.json({'message': 'patient created', newPatient});
+		const newItem = await this.prismaEntity.create({data: body});
+		response.json({'message': `${this.entity.toUpperCase()} created`, newItem});
 	}
 
 	async update (request,response) {
 		const {id} = request.params;
 		const {body} = request;
 
-		const patient = await prisma.patient.update({
+		const item = await this.prismaEntity.update({
 			where: {id},
 			data: body,
 		});
-		response.json({'message': 'patient updated', patient});
+		response.json({'message': `${this.entity.toUpperCase()} updated`, item});
 	}
 
 }
