4562bb9aeb516e071686e195e76d6781c2faf8d3
maangelos <mariannaangelos.alves@gmail.com>
2022-04-24 17:44:15 -0300

    add uniques controlles and routes

---
+++ src/controllers/SchedualingController.js
@@ -0,0 +1,9 @@
+import Controller from './Controller.js';
+
+class ScheadulingController extends Controller{
+	constructor() {
+		super('Schedualing');
+	}
+}
+
+export default ScheadulingController;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,21 @@
 import {Router} from 'express';
-import Controller from '../controllers/Controller.js';
+import PatientController from '../controllers/PatientController.js';
+import ScheadulingController from '../controllers/SchedualingController.js';
 
-const controller = new Controller();
+const patientController = new PatientController();
+const scheadulingController = new ScheadulingController();
 
 const router = Router();
 
-router.get('/patients', controller.index);
-router.get('/patient/:id', controller.getOne);
-router.post('/patient', controller.store);
-router.put('/patient/:id', controller.update);
+router.get('/patient', patientController.index.bind(patientController));
+router.get('/patient/:id', patientController.getOne.bind(patientController));
+router.post('/patient', patientController.store.bind(patientController));
+router.put('/patient/:id', patientController.update.bind(patientController));
+
+router.get('/scheaduling', scheadulingController.index.bind(scheadulingController));
+router.get('/scheaduling/:id', scheadulingController.getOne.bind(scheadulingController));
+router.post('/scheaduling', scheadulingController.store.bind(scheadulingController));
+router.put('/scheaduling/:id', scheadulingController.update.bind(scheadulingController));
 
 export default router;
 
