043516f565a8edafcaa02a9e166f0413be9f956a
maangelos <mariannaangelos.alves@gmail.com>
2022-04-24 16:38:55 -0300

    att index.js (import route)

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
+import router from './routes/index.js';
 
 dotenv.config();
 
@@ -14,6 +15,8 @@ app.use(express.json());
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
+
+app.use('/api',router);
 
 app.get('/', (required,response) => response.json({message: 'Running'}));
 app.listen(PORT, () => {
