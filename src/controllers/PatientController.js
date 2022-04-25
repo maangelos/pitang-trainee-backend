7af15c830c3fc86c94a975bc655f2efc56a3e1b3
maangelos <mariannaangelos.alves@gmail.com>
2022-04-24 22:29:31 -0300

    add patient validation

--- src/controllers/PatientController.js
+++ src/controllers/PatientController.js
@@ -1,8 +1,14 @@
+import Joi from 'joi';
 import Controller from './Controller.js';
+
+const schema = Joi.object({
+	name: Joi.string().required(),
+	birthDate: Joi.date().required(),
+});
 
 class PatientController extends Controller{
 	constructor() {
-		super('patient');
+		super('patient', schema);
 	}
 }
 
