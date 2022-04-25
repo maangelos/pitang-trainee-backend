09303e24b7fac774775283ed49625690bf07f4a9
maangelos <mariannaangelos.alves@gmail.com>
2022-04-24 23:32:25 -0300

    add scheaduling validation

--- src/controllers/SchedualingController.js
+++ src/controllers/SchedualingController.js
@@ -1,8 +1,41 @@
+import Joi from 'joi';
 import Controller from './Controller.js';
+import prisma from '@prisma/client';
 
+const { VaccinationStatus } = prisma;
+
+const schema = Joi.object({
+	date: Joi.date().required(),
+	status: Joi.string().required().valid(
+		...Object.values(VaccinationStatus)
+	),
+	patient: Joi.object({
+		connect: Joi.object({
+			id: Joi.string().required()
+		})
+	}),
+});
 class ScheadulingController extends Controller{
 	constructor() {
-		super('Schedualing');
+		super('Schedualing', schema);
+	}
+
+	store(request,response){
+
+		const patientId = request.body.patientId;
+
+		delete request.body.patientId;
+
+		request.body = {
+			...request.body,
+			patient: {
+				connect: {
+					id: patientId
+				}
+			}
+		};
+
+		super.store(request,response);
 	}
 }
 
